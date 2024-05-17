00de2ebb7284340f3461ad6fd8dc1d50d2104bc7
Nazarii Stakhovskyi <Nazar Stakhovskyi>
2024-05-17 21:12:48 +0300

    add solution (dynamic)

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # goit-algo-hw-09
+
+## Task:
+
+1. Find the optimum solution for the change (knapsack) problem (give an amount of change given a set of coins) using a `greedy` approach as well as `dynamic` programming;
+2. Solve the problem for a larger change amount (**20,000** cents in this experiment);
+3. Time both algorithms;
+
+## Results:
+
+### Total coins (GREEDY): {50: 400, 25: 0, 10: 0, 5: 0, 2: 0, 1: 0}
+
+- Total change assembled GREEDY: 20000
+- Execution time (GREEDY): 0.001
+
+### Total coins (DYNAMIC): {50: 400, 25: 0, 10: 0, 5: 0, 2: 0, 1: 0}
+
+- Total change assembled DYNAMIC: 20000
+- Execution time (DYNAMIC): 8.24
+
+The following results can be observed:
+
+- both algorithms solved the task successfully - the required change amount is assmbled in both cases:
+- however, the `greedy` algortithm completed the task markedly faster - within 0.001 sec (as compared to `dynamic` - 8.24 sec);
+- at the same time, both algorithms work comparably fast with tasks that involve smaller numbers (to assemble 200 cents of change) - 0 sec for `greedy` VS 0.002 sec for `dynamic`;
+- the main source of such delays when it comes to applying an algorithm based on `dynamic` programming to knapsack problems seems to be the fact that the sum of the choice items from which the knapsack is to be filled must be at least equal to the knapsack volume (unless the memory table construction and fillup is **heavily modified**, the algorithm will fail to find the optimum solution (e.g. if the knapsack can carry 20 kg, but the sum of the choice items equals 10 kg. In such a case, the `dynamic` algorithm will simply put everything inside. `Greedy` algorithms are more flexible as they can be modified fairly easily)).
+- additionally, the implementation of greedy algorithm is much easier to read as it is straightforward (compared to the memo tables that dynamic programming relies on)
--- task_1.py
+++ task_1.py
@@ -1,33 +1,56 @@
+import time
+
 def greedy_algorithm(coins: list, total_sum: int):
+    total_sum_original = total_sum
     total_coins = {}
     for coin in coins:
-        num_coins = total_sum // coin
+        num_coins = total_sum // coin # <-- very easy to optimize
         total_sum -= coin * num_coins
         total_coins.update({coin : num_coins})
+    print("Total change assembled GREEDY:", total_sum_original - total_sum)
     return total_coins
 
 def dynamic_programming(coins: list, total_sum: int):
+    original_coins = coins
+    while sum(coins) < total_sum:
+        coins.extend(original_coins)
+        coins = sorted(coins, reverse = True)
     n = len(coins)
 
     K = [[0 for c in range(total_sum + 1)] for i in range(n + 1)]
 
-    for i in range(n + 1):
-        for w in range(total_sum + 1):
+    for w in range(total_sum + 1):
+        
+        for i in range(n + 1):
             if i == 0 or w == 0:
                 K[i][w] = 0
             elif coins[i - 1] <= w:
-                K[i][w] = max(K[i - 1][w - coins[i - 1]], K[i - 1][w])
+                print(coins[i - 1], K[i - 1][w - coins[i - 1]], K[i - 1][w])
+                mx = max(coins[i - 1] + K[i - 1][w - coins[i - 1]], K[i - 1][w]) # <-- extremely difficult to optimize
+                K[i][w] = mx
             else:
                 K[i][w] = K[i - 1][w]
-    for i in K:
-        print(i)
-    return K[n][total_sum]
+
+    dyn_total = K[n][total_sum]
+    print("Total change assembled DYNAMIC:", dyn_total)
+    total_coins = {}
+    n = 0
+    while coins:
+        num_coins = dyn_total // coins[n]
+        total_coins.update({coins[n] : num_coins})
+        dyn_total -= num_coins * coins[n]
+        coins = [c for c in coins if c != coins[n]]
+    return total_coins
 
 if __name__ == "__main__":
     coins = [50, 25, 10, 5, 2, 1]
-    
-    total_coins_greedy = greedy_algorithm(coins, 131)
-    print("Total coins (GREEDY):", total_coins_greedy)
 
-    total_coins_dynamic = dynamic_programming(coins, 131)
-    print("Total calories (DYNAMIC):", total_coins_dynamic)
+    time_now = time.time()
+    total_coins_greedy = greedy_algorithm(coins, 87)
+    print("Total coins (GREEDY):", total_coins_greedy)
+    print("Execution time (GREEDY):", time.time() - time_now)
+
+    time_now = time.time()
+    total_coins_dynamic = dynamic_programming(coins, 87)
+    print("Total coins (DYNAMIC):", total_coins_dynamic)
+    print("Execution time (DYNAMIC):", time.time() - time_now)
